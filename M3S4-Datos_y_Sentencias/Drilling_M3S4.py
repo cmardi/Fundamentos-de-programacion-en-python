27e63d6c3175372e33e6f845665a7f244d78977d
Carlos Mamani Ardiles <cmamani2003@gmail.com>
2024-09-10 02:14:00 -0300

    Agregando carpeta M3S4 al repositorio

---
+++ M3S4-Datos_y_Sentencias/Drilling_M3S4.py
@@ -0,0 +1,21 @@
+persona1 = {
+    "nombre": "Pedro",
+    "apellido": "Pérez",
+    "teléfono": "999999999"
+}
+
+persona2 = {
+    "nombre": "Juan",
+    "apellido": "Díaz",
+    "teléfono": "888888888"
+}
+
+persona3 = {
+    "nombre": "Lisa",
+    "apellido": "Cortés",
+    "teléfono": "777777777"
+}
+
+registro_de_datos = [persona1, persona2, persona3]
+
+print(registro_de_datos)
