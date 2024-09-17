e99533dde93fb964396bb5fcbba005bf53b57c48
Carlos Mamani Ardiles <cmamani2003@gmail.com>
2024-09-17 02:00:18 -0300

    Actualización final

--- M3-Practica_de_Consolidacion/M3-Consolidacion-Filtrando_Datos.py
+++ M3-Practica_de_Consolidacion/M3-Consolidacion-Filtrando_Datos.py
@@ -1,45 +1,16 @@
 """
-Nota: Todos los comentarios añadidos son alternativas funcionales del código.
+Nota: Los comentarios añadidos al final corresponden al formato inicial y es totalmente funcional.
 
 """
-nombres = [
-    "Harry Houdini",
-    "Newton",
-    "David Blaine",
-    "Hawking",
-    "Messi",
-    "Teller",
-    "Einstein",
-    "Pele",
-    "Juanes"
-]
+nombres = ["Harry Houdini", "Newton", "David Blaine", "Hawking", "Messi", "Teller", "Einstein", "Pele", "Juanes"]
 
-nombres_magos = {0, 2, 5}
-nombres_cientificos = {1, 3, 6}
-
-# for nombre in range(len(nombres)):          
-#     if nombre in nombres_magos:
-#         magos.append(nombres[nombre])
-#     elif nombre in nombres_cientificos:
-#         cientificos.append(nombres[nombre])
-#     else:
-#         otros.append(nombres[nombre])
+nombres_magos, nombres_cientificos = {0, 2, 5}, {1, 3, 6}
 
 magos= [nombres[i] for i in range(len(nombres)) if i in nombres_magos]
 cientificos = [nombres[i] for i in range(len(nombres)) if i in nombres_cientificos]
 otros = [nombres[i] for i in range(len(nombres)) if i not in nombres_magos and i not in nombres_cientificos]
 
-# print()
-# print(magos)
-# print(cientificos)
-# print(otros)
-# print()
-
 print(f"\nMagos: {magos} \nCientíficos: {cientificos} \nOtros: {otros}\n")
-
-# def hacer_grandioso(magos):
-#     for nombre in range(len(magos)):
-#         print("El gran " + magos[nombre])
 
 def hacer_grandioso(*args):
     for args in magos:
@@ -54,6 +25,43 @@ def imprimir_nombres(*args):
 
 hacer_grandioso(magos)
 imprimir_nombres(nombres, magos, cientificos, otros) 
+
+# nombres = [
+#     "Harry Houdini",
+#     "Newton",
+#     "David Blaine",
+#     "Hawking",
+#     "Messi",
+#     "Teller",
+#     "Einstein",
+#     "Pele",
+#     "Juanes"
+# ]
+
+# magos = []
+# cientificos = []
+# otros = []
+
+# nombres_magos = {0, 2, 5}
+# nombres_cientificos = {1, 3, 6}
+
+# for nombre in range(len(nombres)):          
+#     if nombre in nombres_magos:
+#         magos.append(nombres[nombre])
+#     elif nombre in nombres_cientificos:
+#         cientificos.append(nombres[nombre])
+#     else:
+#         otros.append(nombres[nombre])
+
+# print()
+# print(f"Magos: ",magos)
+# print(f"Científicos: ",cientificos)
+# print(f"Otros: ",otros)
+# print()
+
+# def hacer_grandioso(magos):
+#     for nombre in range(len(magos)):
+#         print("El gran " + magos[nombre])
 
 # def imprimir_nombres(*args):
 #     print("\nTodos los nombres antes de ser modificados:\n")
@@ -71,4 +79,4 @@ imprimir_nombres(nombres, magos, cientificos, otros)
 #     print()
     
 # hacer_grandioso(magos)
-# imprimir_nombres(nombres, magos, cientificos, otros)       
+# imprimir_nombres(nombres, magos, cientificos, otros)  
