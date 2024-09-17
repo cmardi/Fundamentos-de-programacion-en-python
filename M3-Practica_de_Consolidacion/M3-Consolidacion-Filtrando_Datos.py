a1f3ba423694714d833bb51a5e5b8c174c1c3171
Carlos Mamani Ardiles <cmamani2003@gmail.com>
2024-09-17 01:16:13 -0300

    Actualizando y simplificando el código

--- M3-Practica_de_Consolidacion/M3-Consolidacion-Filtrando_Datos.py
+++ M3-Practica_de_Consolidacion/M3-Consolidacion-Filtrando_Datos.py
@@ -1,3 +1,7 @@
+"""
+Nota: Todos los comentarios añadidos son alternativas funcionales del código.
+
+"""
 nombres = [
     "Harry Houdini",
     "Newton",
@@ -10,10 +14,6 @@ nombres = [
     "Juanes"
 ]
 
-magos = []
-cientificos = []
-otros = []
-
 nombres_magos = {0, 2, 5}
 nombres_cientificos = {1, 3, 6}
 
@@ -25,35 +25,50 @@ nombres_cientificos = {1, 3, 6}
 #     else:
 #         otros.append(nombres[nombre])
 
-#Lista por comprención:
 magos= [nombres[i] for i in range(len(nombres)) if i in nombres_magos]
 cientificos = [nombres[i] for i in range(len(nombres)) if i in nombres_cientificos]
 otros = [nombres[i] for i in range(len(nombres)) if i not in nombres_magos and i not in nombres_cientificos]
 
-print()
-print(magos)
-print(cientificos)
-print(otros)
-print()
+# print()
+# print(magos)
+# print(cientificos)
+# print(otros)
+# print()
 
-def hacer_grandioso(magos):
-    for nombre in range(len(magos)):
-        print("El gran " + magos[nombre])
+print(f"\nMagos: {magos} \nCientíficos: {cientificos} \nOtros: {otros}\n")
 
-def imprimir_nombres(nombres, magos, cientificos, otros):
-    print("\nTodos los nombres antes de ser modificados:\n")
-    for nombre in nombres:
-        print("* " +nombre)
-    print("\nLos nombres de los magos grandiosos:\n")
-    for nombre in magos:
-        print("* " +nombre)
-    print("\nLos nombres de los científicos:\n")
-    for nombre in cientificos:
-        print("* " +nombre)
-    print("\nLos nombres restantes:\n")    
-    for nombre in otros:
-        print("* " +nombre)
+# def hacer_grandioso(magos):
+#     for nombre in range(len(magos)):
+#         print("El gran " + magos[nombre])
+
+def hacer_grandioso(*args):
+    for args in magos:
+        print("El gran " + args)
+
+def imprimir_nombres(*args):
+    print("\nTodos los nombres antes de ser modificados:\n", *[f"* {args}" for args in nombres], sep="\n")
+    print("\nLos nombres de los magos grandiosos:\n", *[f"* {args}" for args in magos], sep="\n")
+    print("\nLos nombres de los científicos:\n", *[f"* {args}" for args in cientificos], sep="\n")
+    print("\nLos nombres restantes:\n", *[f"* {args}" for args in otros], sep="\n")
     print()
 
 hacer_grandioso(magos)
-imprimir_nombres(nombres, magos, cientificos, otros)       
+imprimir_nombres(nombres, magos, cientificos, otros) 
+
+# def imprimir_nombres(*args):
+#     print("\nTodos los nombres antes de ser modificados:\n")
+#     for args in nombres:
+#         print("* " + args)
+#     print("\nLos nombres de los magos grandiosos:\n")
+#     for args in magos:
+#         print("* " + args)
+#     print("\nLos nombres de los científicos:\n")
+#     for args in cientificos:
+#         print("* " + args)
+#     print("\nLos nombres restantes:\n")    
+#     for args in otros:
+#         print("* " + args)
+#     print()
+    
+# hacer_grandioso(magos)
+# imprimir_nombres(nombres, magos, cientificos, otros)       
